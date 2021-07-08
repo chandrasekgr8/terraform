3283ebc40e6804bec0e57edfa71a3b57665a8e57
chandrasekgr8 <71812374+chandrasekgr8@users.noreply.github.com>
2021-07-08 07:46:12 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region  = var.region
+}
+resource "aws_vpc" "my_vpc"{
+    cidr_block                       = "10.0.0.0/20"
+    tags                             = {
+        "Env"  = "poc"
+        "Name" = "my_vpc1"
+    }
+ 
+}
